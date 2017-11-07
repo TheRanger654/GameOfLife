56e6df4a31b09c0de092c0abb2c01d0b93f41bd9
TheRanger654 <han.reemer@gmail.com>
2017-11-07 20:39:44 +0100

    Every Shit
    Vergeten git aan te maken dus beter laat dan nooit.

---
+++ Game of life - kopie/Assets/Scripts/Grit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grit : MonoBehaviour {
+
+    private GameObject[,] grid;
+    public int Columns;
+    public int Rows;
+    public GameObject rect;
+
+    void Start()
+    {
+        grid = new GameObject[Columns, Rows];
+
+        for (int i = 0; i < Columns; i++)
+        {
+            for (int j = 0; j < Rows; j++)
+            {
+                grid[i, j] = Instantiate(rect, new Vector3(transform.position.x + i, transform.position.y + j, transform.position.z), Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Game of life - kopie/Assets/Scripts/GritSelect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GritSelect : MonoBehaviour {
+
+    public void SwitchColor()
+    {
+        if (gameObject.GetComponent<Renderer>().material.color == Color.white)
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.black;
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.white;
+        }
+    }
+}
